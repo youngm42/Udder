868ea63b7626f854d7f4b37f2b5d5bf1a22efe24
Matt Young <youngmatt519@gmail.com>
2022-01-14 11:09:38 -0500

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Matthew Young on 1/14/22.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
