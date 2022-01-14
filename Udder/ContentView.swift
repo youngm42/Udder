25beba16d7ac8f26cd8d1e81c72cdf81bd449dd9
Matt Young <youngmatt519@gmail.com>
2022-01-14 11:21:48 -0500

    Add Title and Subtitle

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+        Text("Udder")
             .padding()
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        Text("Fresh Milk On Demand")
+            .font(.subheadline)
+        }
     }
 }
 
